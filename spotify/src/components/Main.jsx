b40a95fcf064ec10ea7790747ef0bb5e66102545
hasham zafar <hasham307@gmail.com>
2021-06-14 10:49:20 +0200

    ...

--- spotify/src/components/Main.jsx
+++ spotify/src/components/Main.jsx
@@ -1,17 +1,52 @@
-import {Component} from 'react'
+import {Container, Card, Button, Row } from 'react-bootstrap'
+import { useState, useEffect} from 'react'
 
 
 
-class Main extends Component {
 
-    componentDidMount= async ()  =>{
-    const response = await fetch('https://striveschool-api.herokuapp.com/api/deezer/album')
-    let album = await response.json().data[0];
-    console.log(album)
-    
-}
- render() {
-    return console.log(response)
+const Main = (props) => {
+
+   const[music, setMusic] = useState([])
+
+   useEffect(()=>{
+    const getMusic =async () =>{
+        let response = await fetch(`https://striveschool-api.herokuapp.com/api/deezer/search?q=queen`)
+        let musicQuery = await response.json()
+        console.log('what is it',musicQuery);
+        setMusic(musicQuery.data)
+
     }
+
+    getMusic()
+   },[])
+
+
+    return ( 
+        <Container id='body'>
+
+        <Row>
+
+        {
+            music.map(song => 
+              <Row className='pt-5'>
+                <Card className='col-md-3 text-white my-2 bg-dark' key={song.id}>
+        <Card.Img variant="top"  src={song.album.cover_medium} alt={song.album.id} onClick={()=> props.history.push('/artist/' + song.artist.id)}  />
+        <Card.Body>
+          <Card.Title>{song.album.title}</Card.Title>
+          <Card.Text>
+           - {song.artist.name} 
+          </Card.Text>
+          <Button variant="primary">Go somewhere</Button>
+        </Card.Body>
+      </Card>
+      </Row>
+                )
+        }
+
+        </Row>
+        </Container>
+
+     )
 }
-export default Main
+ 
+export default Main;
